bc2f913ac28a8d1f946a09326cadd2f719ecca07
ari shtipi <arish186777@gmail.com>
2023-08-31 16:50:02 +0300

    sections 4  titel card

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import Text from "./components/Text/Text";
 
 const cardData = [
   {
-    title: "Card 1",
+    title: "Card ",
     imageUrl: "https://loremflickr.com/320/240/brazil,rio",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
-    title: "Card 2",
+    title: "Card ",
     imageUrl: "https://loremflickr.com/320/240/paris",
     text: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
-    title: "Card 3",
+    title: "Card ",
     imageUrl: "https://loremflickr.com/320/240/nature",
     text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
@@ -28,7 +28,7 @@ function App() {
         {cardData.map((card, index) => (
           <Card
             key={index}
-            title={card.title}
+            title={card.title + `${index + 1}`}
             imageUrl={card.imageUrl}
             text={card.text}
           />
